7b1452071c94be260d1b76f18c7dad34b7252522
BrayanDesarrollo6 <desarrollo6@hq5.com.co>
2024-02-09 18:12:32 -0500

    Se añade consultas query

--- src/Lib/functionsWoztell.js
+++ src/Lib/functionsWoztell.js
@@ -5,18 +5,18 @@ export async function consultRecordWz(id, externalId, app) {
     try {
         const response = await queryWz.wzRecordExistsByMemberId(id);
 
-        if (!response) {
-            await queryWz.beginTransaction();
+        if (!response.exists) {
 
             try {
                 const insert = await queryWz.wzRecordInsert(id, externalId, app);
-                const consult = await queryWz.wzRecordExistsByMemberId(insert.id);
-                await queryWz.commitTransaction();
+                if (!insert || !insert.id) {
+                    throw new Error('Error en la inserción de registros en wz');
+                }
+                const consult = await queryWz.wzRecordExistsById(insert.id);
                 return consult;
 
-            } catch (insertError) {
-                await queryWz.rollbackTransaction();
-                throw insertError;
+            } catch (error) {
+                throw error;
             }
             
         } else {
