af06bb3a989021957d7d30ec88da51064ce7908c
BrayanDesarrollo6 <desarrollo6@hq5.com.co>
2024-02-23 14:27:58 -0500

    Incluye la query de centro de costo con ciudad

--- src/Database/costCenter.query.js
+++ src/Database/costCenter.query.js
@@ -5,8 +5,21 @@ import { dbConnection } from './connection.js';
 // Función para verificar si existe un registro en la tabla centro_costo por id cliente
 export const costCenterRecordExistsByIdCustomerIdCity = async (customerId, cityId) => {
     try {
-        const { results } = await dbConnection.query('SELECT id AS id2, CAST(zh_id AS CHAR) AS id, nombre AS nombre, dias_pago AS dias_pago, periodicidad_pago AS periodicidad FROM centro_costo WHERE zh_cliente = ?', [customerId]);
+        const { results } = await dbConnection.query(`
+            SELECT 
+                id AS id2, 
+                CAST(zh_id AS CHAR) AS id, 
+                nombre AS nombre, 
+                dias_pago AS dias_pago, 
+                periodicidad_pago AS periodicidad 
+            FROM 
+                centro_costo 
+            WHERE 
+                zh_cliente = ? AND estado = '1' AND zh_ciudad = ?;
+        `, [customerId, cityId]);
+
         return results;
+
     } catch (error) {
         console.error('Error en la consulta de: costCenterRecordExistsByIdCustomerIdCity', error);
         throw error;
