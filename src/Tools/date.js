57b8c5e647505beefe0f86dfc070f363dee24962
BrayanDesarrollo6 <desarrollo6@hq5.com.co>
2024-03-06 11:40:46 -0500

    Edit date format

--- src/Tools/date.js
+++ src/Tools/date.js
@@ -2,5 +2,5 @@
 export const formatDate = (dateIn) => {
     const date = new Date(dateIn);
     const optionDate = { day: '2-digit', month: '2-digit', year: 'numeric' };
-    return date.toLocaleDateString('en-GB', optionDate).replace(/ /g, '-');
+    return date.toLocaleDateString('es-CO', optionDate).replace(/ /g, '-');
 };
