1a79d563a26fa92a1f1e7096e24f8d398c585849
BrayanDesarrollo6 <desarrollo6@hq5.com.co>
2024-03-08 17:04:31 -0500

    Optimizacion del codigo payrollPayment.controller.js

--- src/Controllers/customers/payrollPayment.controller.js
+++ src/Controllers/customers/payrollPayment.controller.js
@@ -2,173 +2,136 @@ import { createErrorResponse, logAndRespond } from '../../Tools/utils.js';
 import { calculateBusinessDays, createDescendingDateMap, createAscendantDateObject, filterBusinessDaysObject } from '../../Lib/Task/payrollDateChange.function.js';
 import { redirectWoztellByMemberId } from '../../Tools/woztell.js';
 import { formatDate } from '../../Tools/date.js';
+import { logWhatsAppCustomerMessages } from '../../Tools/zoho.js';
+import { consultRecordWz } from '../../Lib/wz.function.js';
+import { consultContactByCelAndCustomer } from '../../Lib/contact.function.js';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env' });
 
-// Funcion para verificar el adelanto del pago de nomina solicitado a traves del bot de whatsapp
-async function verifyPayrollPayment(req, res) {
+async function verifyPayroll(req, res, type) {
     try {
         const { member, payrollDate, customer } = req.body;
         
-        console.log("Verificacion pago fecha nomina");
+        console.log(`Verificacion ${type} pago fecha nomina`);
         console.log(payrollDate);
         console.log(customer);
 
-        // Verificar la existencia de 'member', 'payrollDate', 'customer'
+        // Validar cuerpo de la solicitud
         if (!member || !payrollDate || !customer) {
-            return logAndRespond(res, 'Clave (member), (payrollDate) o (customer) no encontrada en el cuerpo de la solicitud', 400);
+            return logAndRespond(res, `Clave (member), (payrollDate) o (customer) no encontrada en el cuerpo de la solicitud`, 400);
         }
 
         // Realizar respuesta a la solicitud
         logAndRespond(res, `Solicitud procesada correctamente`, 200);
+
+        // Consultar datos recibidos en la solicitud
+        const { _id: memberId, externalId, app } = member;
+        const wz_id = await consultRecordWz(memberId, externalId, app);
 
         // Crear objeto fechas y procesar los dias de diferencia entre ellos
         const currentDate = new Date();
         const [day, month, year] = payrollDate.split('/').map(Number);
         const payrollDatePay = new Date(year, month - 1, day);
+        const dateData = type === 'adelanto' ? createDescendingDateMap(payrollDatePay, 5) : createAscendantDateObject(payrollDatePay, 5);
         const businessDaysDifference = calculateBusinessDays(currentDate, payrollDatePay);
 
-        // Redireccionar al cliente al nodo para informar que no es posible modificar el pago de nomina
-        if(businessDaysDifference < 5){
-            const response = await redirectWoztellByMemberId(process.env.WZ_NODE_NOT_ADV_PAY_PAYMENT, '65c3fb10c8d09d00086daef3');
+        // Redireccionar al cliente al nodo para informar que no es posible modificar el pago de nomina para adelantarlo
+        if (type === 'adelanto' && businessDaysDifference < 5) {
+            const response = await redirectWoztellByMemberId(process.env.WZ_NODE_NOT_ADV_PAY_PAYMENT, wz_id.memberId);
             console.log(response);
             return;
         }
 
-        // Procesar la data
-        const dateData = createDescendingDateMap(payrollDatePay, 5);
-        const dateMap = Object.fromEntries([...dateData.entries()]);
-        const message = [...dateData.entries()].map(([id, date], index) => `${index + 1}️⃣  ${date}`).join('\n');
-        
-        // Redireccionar al cliente al nodo para informar las posibles fechas de adelanto de pago
-        const response = await redirectWoztellByMemberId(process.env.WZ_NODE_ADV_PAY_PAYMENT, '65c3fb10c8d09d00086daef3', {
-            dateAdvPay: { ...dateMap },
-            messageAdvPay: message,
+        // Redireccionar al cliente al nodo correspondiente
+        const node = type === 'adelanto' ? process.env.WZ_NODE_ADV_PAY_PAYMENT : process.env.WZ_NODE_DEL_PAY_PAYMENT;
+        const dateDataObj = type === 'adelanto' ? dateData : filterBusinessDaysObject(dateData);      
+        const dateMap = Object.fromEntries([...dateDataObj.entries()]);
+        const message = [...dateDataObj.entries()].map(([id, date], index) => `${index + 1}️⃣  ${date}`).join('\n');
+
+        const response = await redirectWoztellByMemberId(node, wz_id.memberId, {
+            [type === 'adelanto' ? 'dateAdvPay' : 'dateDelPay']: { ...dateMap },
+            [type === 'adelanto' ? 'messageAdvPay' : 'messageDelPay']: message,
         });
+
         console.log(response);
         return;
 
     } catch (error) {
-        console.error('Error en verificar el pago de nomina del cliente', error);
+        console.error(`Error en verificar el ${type} del pago de nomina del cliente`, error);
         const errorResponse = createErrorResponse('Error interno del servidor', 500);
         res.status(errorResponse.statusCode).json(errorResponse);
     }
 }
 
-// Funcion para adelantar el pago de nomina solicitado a traves del bot de whatsapp
-async function advancePayrollPayment(req, res) {
+async function processPayrollChange(req, res, type) {
     try {
         const { member, payrollDateChange, customer } = req.body;
 
-        console.log("Adelanto pago fecha nomina");
+        console.log(`${type} pago fecha nomina`);
         console.log(payrollDateChange);
         console.log(customer);
 
-        // Verificar la existencia de 'member', 'payrollDateChange' y 'customer'
+        // Validar cuerpo de la solicitud
         if (!member || !payrollDateChange || !customer) {
-            return logAndRespond(res, 'Clave (member), (payrollDateChange) o (customer) no encontrada en el cuerpo de la solicitud', 400);
+            return logAndRespond(res, `Clave (member), (payrollDateChange) o (customer) no encontrada en el cuerpo de la solicitud`, 400);
         }
 
         // Realizar respuesta a la solicitud
         logAndRespond(res, `Solicitud procesada correctamente`, 200);
+
+        // Consultar datos recibidos en la solicitud
+        const { _id: memberId, externalId, app } = member;
+        const wz_id = await consultRecordWz(memberId, externalId, app);
+        const cel = parseInt(wz_id.externalId.substring(2));
+        const { id: contactId } = await consultContactByCelAndCustomer(cel, customer);
 
         // Procesar la fecha recibida
         const [day, month, year] = payrollDateChange.split('/').map(Number);
         const payrollDate = new Date(year, month - 1, day);
         const getPayrollDateChange = formatDate(payrollDate);
 
-        // Ticket a compensacion para notificar el adelanto de pago de nomina del cliente al crear registro en el reporte de whatsapp
-        
         // Redireccionar al cliente al nodo donde confirma que se notifico acerca del cambio de la fecha
-        const response = await redirectWoztellByMemberId(process.env.WZ_NODE_NOTIF_ADV_PAY_PAYMENT, '65c3fb10c8d09d00086daef3');
-        console.log(response);
-        return;
+        const response = await redirectWoztellByMemberId(process.env.WZ_NODE_NOTIF_ADV_DEL_PAY_PAYMENT, wz_id.memberId);
+
+        // Ticket a compensación para notificar el cambio de fecha al crear registro en el reporte de WhatsApp
+        if (response) {
+            const messageData = {
+                contactId: contactId,
+                request: `${type} pago nomina`,
+                type: 'Entrada',
+                description: `El cliente solicito ${type} el pago de nomina a través de WhatsApp para la fecha: ${getPayrollDateChange}`,
+                whatsappMemberId: response.member,
+                requestStatus: '1'
+            };
+            await logWhatsAppCustomerMessages(messageData);
+        }
 
     } catch (error) {
-        console.error('Error en adelantar el pago de nomina del cliente', error);
+        console.error(`Error en ${type} el pago de nomina del cliente`, error);
         const errorResponse = createErrorResponse('Error interno del servidor', 500);
         res.status(errorResponse.statusCode).json(errorResponse);
     }
 }
 
-// Funcion para verificar el retraso del pago de nomina solicitado a traves del bot de whatsapp
+// Uso de la función para verificar adelanto de pago
+async function verifyPayrollPayment(req, res) {
+    await verifyPayroll(req, res, 'adelanto');
+}
+
+// Uso de la función para verificar retraso de pago
 async function verifyPayrollDelay(req, res) {
-    try {
-        const { member, payrollDate, customer } = req.body;
-        
-        console.log("Verificacion retraso pago fecha nomina");
-        console.log(payrollDate);
-        console.log(customer);
-
-        // Verificar la existencia de 'member', 'payrollDate', 'customer'
-        if (!member || !payrollDate || !customer) {
-            return logAndRespond(res, 'Clave (member), (payrollDate) o (customer) no encontrada en el cuerpo de la solicitud', 400);
-        }
-
-        // Realizar respuesta a la solicitud
-        logAndRespond(res, `Solicitud procesada correctamente`, 200);
-
-        // Crear objeto fechas
-        const [day, month, year] = payrollDate.split('/').map(Number);
-        const payrollDatePay = new Date(year, month - 1, day);
-
-        // Procesar la data
-        const dateDataObj = createAscendantDateObject(payrollDatePay, 5);
-        const dateData = filterBusinessDaysObject(dateDataObj);
-        const dateMap = Object.fromEntries([...dateData.entries()]);
-        const message = [...dateData.entries()].map(([id, date], index) => `${index + 1}️⃣  ${date}`).join('\n');
-        
-        // Redireccionar al cliente al nodo para informar las posibles fechas de retraso de pago
-        const response = await redirectWoztellByMemberId(process.env.WZ_NODE_DEL_PAY_PAYMENT, '65c3fb10c8d09d00086daef3', {
-            dateDelPay: { ...dateMap },
-            messageDelPay: message,
-        });
-
-        console.log(response);
-        return;
-
-    } catch (error) {
-        console.error('Error en verificar el retraso del pago de nomina del cliente', error);
-        const errorResponse = createErrorResponse('Error interno del servidor', 500);
-        res.status(errorResponse.statusCode).json(errorResponse);
-    }
+    await verifyPayroll(req, res, 'retraso');
 }
 
-// Funcion para retrasar el pago de nomina solicitado a traves del bot de whatsapp
+// Funcion específica para adelantar el pago de nómina
+async function advancePayrollPayment(req, res) {
+    await processPayrollChange(req, res, 'Adelantar');
+}
+
+// Funcion específica para retrasar el pago de nómina
 async function delayPayrollPayment(req, res) {
-    try {
-        const { member, payrollDateChange, customer } = req.body;
-
-        console.log("Retraso pago fecha nomina");
-        console.log(payrollDateChange);
-        console.log(customer);
-
-        // Verificar la existencia de 'member', 'payrollDateChange' y 'customer'
-        if (!member || !payrollDateChange || !customer) {
-            return logAndRespond(res, 'Clave (member), (payrollDateChange) o (customer) no encontrada en el cuerpo de la solicitud', 400);
-        }
-
-        // Realizar respuesta a la solicitud
-        logAndRespond(res, `Solicitud procesada correctamente`, 200);
-
-        // Procesar la fecha recibida
-        const [day, month, year] = payrollDateChange.split('/').map(Number);
-        const payrollDate = new Date(year, month - 1, day);
-        const getPayrollDateChange = formatDate(payrollDate);
-
-        // Ticket a compensacion para notificar el retraso de pago de nomina del cliente al crear registro en el reporte de whatsapp
-        
-        // Redireccionar al cliente al nodo donde confirma que se notifico acerca del cambio de la fecha
-        const response = await redirectWoztellByMemberId(process.env.WZ_NODE_NOTIF_ADV_PAY_PAYMENT, '65c3fb10c8d09d00086daef3');
-        console.log(response);
-        return;
-
-    } catch (error) {
-        console.error('Error en retrasar el pago de nomina del cliente', error);
-        const errorResponse = createErrorResponse('Error interno del servidor', 500);
-        res.status(errorResponse.statusCode).json(errorResponse);
-    }
+    await processPayrollChange(req, res, 'Retrasar');
 }
 
 // Controlador para adelanto de nomina solicitado a traves del bot de whatsapp
